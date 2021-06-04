761e5cbb60a11884c2088294e0b2897d3034ed33
Steve Collins <steve.collins@camc.com>
2021-06-04 15:58:44 +0100

    Code to support blog post

---
+++ DebuggingSourceGenerators/SimpleCodeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace DebuggingSourceGenerators
+{
+    [Generator]
+    public class SimpleCodeGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+#if DEBUGGENERATOR
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Launch();
+            }
+#endif
+        }
+
+
+        public void Execute(GeneratorExecutionContext context)
+    {
+            context.AnalyzerConfigOptions.GlobalOptions.TryGetValue($"build_property.RootNamespace", out var currentNamespace);
+
+            currentNamespace = string.IsNullOrWhiteSpace(currentNamespace) ? "CodeHelper" : currentNamespace;
+            var sourceCode = $@"namespace {currentNamespace}
+{{
+    public static class Messages
+    {{
+
+        public static string SayHello()
+        {{
+            return ""Hello"";
+        }}
+    }}
+}}";
+            context.AddSource("SayHello.g.cs", sourceCode);
+        }
+    }
+}
---
+++ MyConsumingProject/MyMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace MyConsumingProject
+{
+    public class MyMessage
+    {
+        private readonly string _name;
+        public MyMessage(string name)
+        {
+            _name = name;
+        }
+
+        public string GetHelloMessage()
+        {
+            return $"{Messages.SayHello()} {_name}";
+        }
+    }
+}
